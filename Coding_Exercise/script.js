355e06495ef969f158f643217a30cc090ec15aee
mpmp <ambros.simon@orbiszlin.cz>
2025-09-29 21:28:09 +0200

    Completely finished. + animations

--- Coding_Exercise/script.js
+++ Coding_Exercise/script.js
@@ -4,16 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const front = document.getElementById('front');
     const back = document.getElementById('back');
 
-    function write(current_cards) {
+    function write(current_cards,direction="none") {
         cards_place.textContent = "";
         current_cards.forEach((card) => {
             cards_place.appendChild(card);
         });
         current_cards.forEach(card => {
-            card.classList.remove('main')
-            card.classList.remove('left');
-            card.classList.remove('right');
+            card.removeAttribute("class");
+            card.classList.add("fs-card");
+            
         });
+        if (direction === "back") {
+            current_cards[0].classList.add('anim-rightRight');
+            current_cards[1].classList.add('anim-mainRight');
+            current_cards[2].classList.add('anim-leftRight');
+        }
+        else if (direction === "front") {
+            
+            current_cards[0].classList.add('anim-leftLeft');
+            current_cards[1].classList.add('anim-mainLeft');
+            current_cards[2].classList.add('anim-rightLeft');
+
+            
+        }
         current_cards[1].classList.add('main');
         current_cards[0].classList.add('left');
         current_cards[2].classList.add('right');
@@ -31,16 +44,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         cards = new_cards;
         
-        write(cards);
+        write(cards,direction);
     }
     
     
     back.addEventListener('click', () => {
-        moveCards("front");
+        moveCards("back");
         
     })
     front.addEventListener('click', () => {
-        moveCards("back");
+        moveCards("front");
         
     })
     write(cards);
